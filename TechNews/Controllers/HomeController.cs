39a20ffe2329b4e996bebeafadad7f29c8feb30b
Huyberkiss2512@gmail.com <Huyberkiss2512@gmail.com>
2020-11-01 16:40:31 +0700

    add login logout and layout for news

--- TechNews/Controllers/HomeController.cs
+++ TechNews/Controllers/HomeController.cs
@@ -5,17 +5,44 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TechNews.DB;
+using TechNews.ViewModel;
 
 namespace TechNews.Controllers {
     public class HomeController : Controller {
+
+        private TechNewsEntities _dbContext;
+
+        public HomeController() {
+            _dbContext = new TechNewsEntities();
+        }
+
+
         public ActionResult Index() {
+
+            var Posts = (from post in _dbContext.Posts
+                         join history in _dbContext.Histories on post.hID equals history.hID
+                         join user in _dbContext.Accounts on history.posterID equals user.aID
+                         select new HomeViewModel() {
+                             Id = post.postID,
+                             Title = post.postTitle,
+                             Content = post.postContent,
+                             PosterName = user.aFullname,
+                             PostedDay = (DateTime)history.dateAccepted
+                         }).ToList<HomeViewModel>();
+
+            ViewBag.posts = Posts;  
+
+
+            ViewBag.category = _dbContext.Categories.ToList<Category>();
+
             return View();
         }
 
-        public ActionResult About() {
+
+        public ActionResult About(string x, string y) {
             ViewBag.Message = "Your application description page.";
-            
-           
+
+
             return View();
         }
 
